7af25dc8a40e90f6163d857a1c63367b882432b8
crisanto-dev <heac.creative@gmail.com>
2020-11-09 00:19:47 -0500

    Add project files.

---
+++ saludo-cpp/saludo-cpp.cpp
@@ -0,0 +1,43 @@
+// saludo-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*Escriba un programa que pida su nombre y lo salude con su nombre*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string nombre;
+	bool bandera = true; 
+	cout << "Por favor ingrese su nombre: ";
+	do
+	{
+		getline(cin, nombre); // Forma de obtener la linea completa
+
+		if (nombre != "")
+		{
+			bandera = false;
+		}
+		else {
+			cout << "Por favor ingrese su nombre: ";
+		}
+	} while (bandera);
+
+	cout << "Bienvenido: " + nombre;
+	cout << "\n";
+	cout << "Fin del programa, nos vemos de nuevo";
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
